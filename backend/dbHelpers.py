6a5b94916a3543e068eedf90e8ad8f390c4c9177
Fabio Hernandez Rubio <44535804+FabioHdez@users.noreply.github.com>
2025-04-01 18:58:59 -0400

    fixes for database

--- backend/dbHelpers.py
+++ backend/dbHelpers.py
@@ -11,19 +11,16 @@ conn = mysql.connector.connect(
 def getUsersChannels(username):
     cursor = conn.cursor()
 
-
     #this statement is vulnerable to sql injection btw
     cursor.execute(f"""
-                    SELECT channels.link, channels.name 
+                    SELECT channels.link, channels.channel_name 
                     FROM channels 
                     LEFT JOIN users ON users.id = channels.owner 
                     WHERE users.username = '{username}' 
                    """)
     rows = cursor.fetchall()
-
     keys = []
     values = []
-
     for row in rows:
         keys.append(row[0])
         values.append(row[1])
